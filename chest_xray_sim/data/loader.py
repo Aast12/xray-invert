e8ab19975f3752c70174f1aeea18e3f73bf7fb3d
Alam Sanchez <asancheztorres1905@gmail.com>
2025-04-16 22:34:17 +0200

    Fix loader

--- chest_xray_sim/data/loader.py
+++ chest_xray_sim/data/loader.py
@@ -19,15 +19,16 @@ from torchvision.transforms import v2
 
 class ChexpertDataset(torchvision.datasets.ImageFolder):
     def __init__(self, root, meta_dir, split=None, **kwargs):
-        super(ChexpertDataset, self).__init__(root=root, **kwargs)
+        data_dir = os.path.join(root, split) if split else root
+        super(ChexpertDataset, self).__init__(root=data_dir, **kwargs)
         self.metadata = pd.read_csv(meta_dir)
-        self.metadata = self.metadata[self.metadata["frontal_lateral"] == "Frontal"]
+        # self.metadata = self.metadata[self.metadata["frontal_lateral"] == "Frontal"]
 
         if split is not None:
             self.metadata = self.metadata[self.metadata["split"] == split]
 
         self.metadata["abs_img_path"] = self.metadata["path_to_image"].apply(
-            lambda x: os.path.join(self.root, x.replace(".jpg", ".png"))
+            lambda x: os.path.join(root, x.replace(".jpg", ".png"))
         )
         self.metadata["exists"] = self.metadata["abs_img_path"].apply(
             lambda x: os.path.exists(x)
@@ -40,15 +41,27 @@ class ChexpertDataset(torchvision.datasets.ImageFolder):
 
         img_path = self.imgs[index][0]
 
-        meta = (
-            self.metadata[self.metadata["abs_img_path"] == img_path].head(1).to_dict()
-        )
+        meta = self.metadata[self.metadata["abs_img_path"] == img_path].head(1)
+        meta = meta.iloc[0].to_dict() if not meta.empty else {}
 
         # make a new tuple that includes original and the path
         return original_tuple + (
             img_path,
             meta,
         )
+
+    @staticmethod
+    def collate(batch):
+        images, labels, paths, meta = zip(*batch)
+
+        images = torch.stack(images)
+
+        # For labels, ensure they're all tensors before stacking
+        if all(isinstance(label, torch.Tensor) for label in labels):
+            labels = torch.stack(labels)
+
+        # Return as a tuple without trying to convert metadata to tensors
+        return images, labels, paths, meta
 
 
 def get_chexpert_dataset(data_dir, meta_dir, split=None, **kwargs):
@@ -66,13 +79,19 @@ def get_chexpert_dataset(data_dir, meta_dir, split=None, **kwargs):
             ]
         ),
     )
-    ds_loader = DataLoader(ds, batch_size=64, shuffle=True)
+    ds_loader = DataLoader(
+        ds,
+        batch_size=12,
+        shuffle=True,
+        collate_fn=ChexpertDataset.collate,
+    )
     return ds_loader
 
 
 if __name__ == "__main__":
     images = get_chexpert_dataset(
-        data_dir="/Volumes/T7/datasets/chexpert_plus/PNG/PNG/train",
+        data_dir="/Volumes/T7/datasets/chexpert_plus/PNG/PNG/",
+        split="train",
         meta_dir="/Volumes/T7/datasets/chexpert_plus/df_chexpert_plus_240401.csv",
     )
 
