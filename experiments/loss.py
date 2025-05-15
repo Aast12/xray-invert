fix segmentation penalty computation

experiments/loss.py
@@ -61,6 +61,7 @@ def compute_single_mask_penalty(
 
     # region_values = txm[mask.astype(bool)]
     region_values = txm * mask
+
     region_size = jnp.sum(mask, axis=(-2, -1))
 
     below_min_capped = jnp.maximum(0.0, min_val - region_values)
@@ -146,7 +147,6 @@ def detached_segmentation_sq_penalty(
 ):
     penalties = jnp.ones((value_ranges.shape[0], txm.shape[0]))
 
-    jax.debug.print("penalties shape: {x}", x=penalties.shape)
     # TODO: possibly improve by making broadcast operations
     for mask_id, val_range in enumerate(value_ranges):
         mask = segmentation[:, mask_id]
@@ -155,19 +155,14 @@ def detached_segmentation_sq_penalty(
         region_values = txm * mask
         region_size = jnp.sum(mask, axis=(-2, -1))
 
-        jax.debug.print("region size: {x}", x=region_size.shape)
-
         below_min = jnp.maximum(0.0, min_val - region_values) ** 2
         above_max = jnp.maximum(0.0, region_values - max_val) ** 2
 
         region_penalty = (below_min + above_max) / (
             jnp.expand_dims(region_size, (1, 2)) + 1e6
         )
-        jax.debug.print("region penalty: {x}", x=region_penalty.shape)
         penalties = penalties.at[mask_id].set(
             jnp.sum(region_penalty, axis=(-2, -1))
         )
-
-    jax.debug.print("penalties shape: {x}", x=penalties.shape)
 
     return penalties

experiments/segmentation.py
@@ -358,7 +358,7 @@ def wandb_experiment(
 
     wandb.log({"samples": samples_tables})
 
-    return txm, weights, pred
+    return txm, weights, pred, segmentations
 
 
 def run_processing(
@@ -396,6 +396,8 @@ def run_processing(
         print("Error during experiment:", e)
         return
 
+    results, segmentations = results[:-1], results[-1]
+
     assert results is not None
     process_results(images, segmentations, meta_batch, value_ranges, results)
 
@@ -425,7 +427,7 @@ def sweep_based_exec(
                 # regularization params, ensure they have some influence in loss
                 "total_variation": {"min": 0.1, "max": 0.5},
                 "prior_weight": {"min": 0.2, "max": 1.0},
-                "gmse_weight": {"min": 0.0, "max": 0.5},
+                "gmse_weight": {"min": 0.0, "max": 1.0},
                 "PRNGKey": {"values": [0, 42]},
                 "tm_init_params": {
                     "values": [
@@ -491,7 +493,7 @@ def single_experiment(
     value_ranges = jnp.array(value_ranges, dtype=DTYPE)
     images = jnp.array(images, dtype=DTYPE).squeeze(1)
 
-    txm, weights, pred = wandb_experiment(
+    txm, weights, pred, segmentations = wandb_experiment(
         images,
         segmentations,
         value_ranges,
@@ -501,7 +503,7 @@ def single_experiment(
         loss_logger=loss_logger,
     )
 
-    seg_labels, segmentations = batch_get_exclusive_masks(segmentations, 0.6)
+    # seg_labels, segmentations = batch_get_exclusive_masks(segmentations, 0.6)
     metrics = batch_evaluation(images, txm, pred, segmentations, value_ranges)
     ssim = metrics.ssim
     psnr = metrics.psnr